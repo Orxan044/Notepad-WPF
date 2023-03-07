c3897b8d0e5fc8140595214db3c9ee1f1ddc4444
Husey_so01@itstep.edu.az <Husey_so01@itstep.edu.az>
2023-03-07 21:22:58 +0400

    Add project files.

---
+++ Notepad WPF/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+
+namespace Notepad_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AutoSave_Checked(object sender, RoutedEventArgs e)
+        {
+            if (AutoSave.IsChecked == true)
+            {
+                File.WriteAllText("BizimFile.txt", textboxyazi.Text);
+
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllText("BizimFile.txt", textboxyazi.Text);
+            textbox.AppendText(Directory.GetCurrentDirectory() + "\\BizimFile.txt");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            textboxyazi.Cut();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            textboxyazi.Copy();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            textboxyazi.Paste();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            textboxyazi.Focus();
+            textboxyazi.SelectAll();
+        }
+
+
+    }
+}
